abfbbebcf3d49b8e1c39b6b91c461d050cbad953
sazal <sazals48@gmail.com>
2025-09-25 16:11:52 +0545

    lucky draw

---
+++ src/app/(admin)/admin/lucky-draw/page.tsx
@@ -0,0 +1,5 @@
+import LuckyDrawView from "@/components/festconfig/luckydraw/lucky-draw-view";
+
+export default function LuckyDrawPage() {
+  return <LuckyDrawView />;
+}
---
+++ src/components/festconfig/luckydraw/lucky-draw-view.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import InfoForm from "@/components/salesfest/InfoForm";
+
+export default function LuckyDrawView() {
+  return <InfoForm initialOrgData={null} />;
+}
